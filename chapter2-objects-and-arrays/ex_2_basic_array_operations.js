d7b5da29cbd5cd81fb2f211c3c7be86db19503b9
Alexandru Ostafi <61746596+alexandruostafi@users.noreply.github.com>
2026-03-09 12:44:33 +0000

    feat: complete ex 2, basic array operations

---
+++ chapter2-objects-and-arrays/ex_2_basic_array_operations.js
@@ -0,0 +1,6 @@
+favoriteGames = ["Dark Souls", "Warhammer 40K: Space Marine 2", "Elden Ring"]
+
+console.log(favoriteGames[0])
+console.log(favoriteGames.length)
+favoriteGames.push("Street Fighter 6")
+console.log(favoriteGames)
