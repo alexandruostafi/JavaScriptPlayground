c7ee235678b7ea2d9639b8dcb4447831ac76b9a0
Alexandru Ostafi <61746596+alexandruostafi@users.noreply.github.com>
2026-03-09 12:43:58 +0000

    feat: complete ex 1, create and access objects

---
+++ chapter2-objects-and-arrays/ex_1_create_and_access_object.js
@@ -0,0 +1,9 @@
+var movie =
+{
+    title: "Inception",
+    year: 2010,
+    rating: 8.8,
+    isWatched: true
+}
+
+console.log(`The movie ${movie.title}(${movie.year}) has a rating of ${movie.rating}. Watched: ${movie.isWatched}`)
