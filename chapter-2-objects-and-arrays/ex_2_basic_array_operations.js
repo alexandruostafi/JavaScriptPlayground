50c1d0f0bdff979a7027bd2cf920bf91a59cc4fa
Alexandru Ostafi <alexostafi98@gmail.com>
2026-03-09 21:18:10 +0200

    feat: complete chapter two exercises

---
+++ chapter-2-objects-and-arrays/ex_2_basic_array_operations.js
@@ -0,0 +1,6 @@
+let favoriteGames = ["Dark Souls", "Warhammer 40K: Space Marine 2", "Elden Ring"]
+
+console.log(`First game: ${favoriteGames[0]}`)
+console.log(`Number of favorite games: ${favoriteGames.length}`)
+favoriteGames.push("Street Fighter 6")
+console.log(`Updated list: ${favoriteGames}`)
