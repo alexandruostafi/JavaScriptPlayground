403762d62c60507e6683a5400cbab46f19642bd9
Alexandru Ostafi <61746596+alexandruostafi@users.noreply.github.com>
2026-03-10 09:43:44 +0000

    feat: complete second exercise to evaluate grades

---
+++ chapter-3-operators-and-control-structures/ex_2_grade_evaluation.js
@@ -0,0 +1,11 @@
+const score = 49
+
+if (score >= 90) {
+    console.log(`${score} -> Excellent`)
+} else if (score >= 75) {
+    console.log(`${score} -> Good`)
+} else if (score >= 50) {
+    console.log(`${score} -> Pass`)
+} else {
+    console.log(`${score} -> Fail`)
+}
